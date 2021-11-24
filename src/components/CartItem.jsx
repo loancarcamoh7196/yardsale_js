807184060d49994001bb654c9bcbe8f9b679d221
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2021-11-24 11:20:48 -0300

    [dev-react] Cambios para que componente CartItem pueda recivbir dato desde API

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import '../styles/CartItem.scss';
+import AppContext from '../context/AppContext';
+import '@styles/CartItem.scss';
 
-const CartItem = () => {
+const CartItem = ( {product} ) => {
     return (
         <div className='cart__item'>
             <figure>
-            <Link to=''>
-                <img src='https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' alt='bike' srcset='' />
-            </Link>
+                <img src={product.images[0]} alt={product.title} srcSet='' />
             </figure>
-            <p>Bike</p>
-            <p>$80.00</p>
+            <p>{product.title}</p>
+            <p>${product.price}</p>
         </div>
     );
 };
