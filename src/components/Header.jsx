abb549dc5f289a1a47e1966553721e8c85a37d36
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2021-11-24 11:26:07 -0300

    [dev-react] Implementacion de useState - togleMenu ; useContext - carritito; usode API externa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import AppContext from '../context/AppContext';
+import MyOrder from '../pages/MyOrder';
 import '@styles/Header.scss';
 import Menu from '@components/Menu';
 
@@ -8,40 +10,45 @@ import logoHeader from '@logos/logo_yard_sale.svg';
 import iconShoppingCart from '@icons/icon_shopping_cart.svg';
 
 const Header = () => {
-    const [toggle, setToggle] = useState(false);
+    const [ toggle, setToggle ] = useState(false);
+    const [ toggleOrders, setToggleOrders ] = useState(false);
+    const { state } = useContext(AppContext);
 
     const handleToggle = () => {
         setToggle(!toggle);// Cambia el estado a la inversa
     };//Despligue de menu
 
     return (
-        <nav>
-        <img src={ iconMenu } alt='menu' className='header__menu' />
-        <div className='header__navbar header__navbar--left'>
-            <img src={ logoHeader } alt='' />
-        
-            <ul>
-                <li><a href='#'>Todo</a></li>
-                <li><a href='#'>Electronica</a></li>
-                <li><a href='#'>Muebles</a></li>
-                <li><a href='#'>Jugetes</a></li>
-                <li><a href='#'>Otros</a></li>
-            </ul>
-        </div>
-        <div className='header__navbar header__navbar--right'>
-            <ul>
-                <li className='navbar__email' onClick={ handleToggle }>
-                    lolovely@something.com
-                </li>
-                <li className='navbar__shopping-cart'>
-                    <img src={ iconShoppingCart } alt='icono carrito' />
-                    <div>2</div>
-                </li>
-            </ul>
-        </div>
-        {toggle && <Menu />}
-        
-    </nav>
+        <>
+            <nav>
+                <img src={ iconMenu } alt='menu' className='header__menu' />
+                <div className='header__navbar header__navbar--left'>
+                    <img src={ logoHeader } alt='' />
+                
+                    <ul>
+                        <li><a href='#'>Todo</a></li>
+                        <li><a href='#'>Electronica</a></li>
+                        <li><a href='#'>Muebles</a></li>
+                        <li><a href='#'>Jugetes</a></li>
+                        <li><a href='#'>Otros</a></li>
+                    </ul>
+                </div>
+                <div className='header__navbar header__navbar--right'>
+                    <ul>
+                        <li className='navbar__email' onClick={ handleToggle }>
+                            lolovely@something.com
+                        </li>
+                        <li className='navbar__shopping-cart' onClick={() => setToggleOrders(!toggleOrders)} >
+                            <img src={ iconShoppingCart } alt='icono carrito' />
+                            { state.cart.length > 0 ? <div> {state.cart.length} </div> : null }
+                        </li>
+                    </ul>
+                </div>
+                { toggle && <Menu /> }
+                
+            </nav>
+            { toggleOrders && <MyOrder /> }
+        </>
     );
 };
 
