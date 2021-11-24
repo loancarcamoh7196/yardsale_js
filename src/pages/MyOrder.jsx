7a961ffd93a009bc04360a565204612d9c9a5f14
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2021-11-24 11:31:18 -0300

    [dev-react] Implementacion de addToCart y Carrito de compra (checkout - my order)

--- src/pages/MyOrder.jsx
+++ src/pages/MyOrder.jsx
@@ -1,30 +1,39 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
 
 import OrderList from '@containers/OrderList';
 import OrderDetail from '@components/OrderDetail';
 import CartItem from '@components/CartItem';
+import '../styles/MyOrder.scss';
 
 
 const MyOrder = () => {
+    const {state} = useContext(AppContext);
+    console.log(state)
     return (
-        <OrderList>
-            <h1 className='order__title'> Mi Orden de Compra</h1>
-                <div className='my-order-content'>
-                    <OrderDetail />
-                </div>
-                
-                <CartItem />
-                <CartItem />
-                <CartItem />
-                <CartItem />
-                <CartItem />
-                <CartItem />
-                <CartItem />
-                <CartItem />
-                <CartItem />
-                <CartItem />
+        <aside className='my-order'>
+            <OrderList>
+                <h1 className='order__title my-order__title'>Carrito de Compra</h1>
+                    <div className='total-order__content'>
+                        {state.cart.length > 0 ? 
+                            <OrderDetail />
+                        :
+                            'Hey, agrega algun producto '
+                        }
+                    </div>
+                    
+                    
+                    {state.cart.map(product => (
+                        <CartItem product={product} key={`orderItem-${product.id}`} />
+                    ))}
+                    
 
-        </OrderList>   
+                    <button className="primary-button my-order__button">
+                        Confirmar pedido
+                    </button>
+            </OrderList>
+        </aside>
+        
     );
 };
 
