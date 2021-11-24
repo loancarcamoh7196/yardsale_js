3741057b184ad121a176136d6607c0f307b33bf8
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2021-11-24 11:30:35 -0300

    [dev-react] Implementacion de useRef dentro de proyecto

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,27 +1,39 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Form from '@containers/Form';
 
 import formIcon from '@logos/logo_yard_sale.svg';
 
 const Login = () => {
+    const form = useRef(null);
+
+    const handleSubmit = ( event ) => {
+        event.preventDefault();
+        const formData = new FormData( form.current );
+        const data = {
+            username: formData.get('email'),
+            password: formData.get('password')
+        }
+        console.log(data);
+    };
+    
     return (
         <Form>
             <img src={ formIcon } alt='logo tienda' className='form__logo' />
 
-            <form action="/" className="form">
+            <form action='/' className='form' ref={ form } >
 
-                <label for="email" className="form__label"> Email </label>
-                <input type="email" name="" id="email" placeholder="lolovely@something.com" className="form__input form__input--email" />
+                <label htmlFor='email' className='form__label'> Email </label>
+                <input type='email' name='email' placeholder='lolovely@something.com' className='form__input form__input--email' />
 
-                <label for="password" className="form__label"> Contraseña </label>
-                <input type="password" name="" id="password" placeholder="**********" className="form__input " />
+                <label htmlFor='password' className='form__label'> Contraseña </label>
+                <input type='password' name='password' placeholder='**********' className='form__input ' />
 
-                <input type="submit" value="Inicia Sesión" className="primary-button form__button" />
+                <button type='submit' value='' className='primary-button form__button' onClick={ handleSubmit } > Inicia Sesión </button>
 
-                <a href="#">Olvide mi contraseña</a>
+                <a href='#'>Olvide mi contraseña</a>
             </form>
 
-            <button className="secundary-button signup__button">
+            <button className='secundary-button signup__button' >
                 Registrarse
             </button>
         </Form>  
